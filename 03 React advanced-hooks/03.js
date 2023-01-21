324c4afa1ae456c519aa5f51adc33fa92fa75b8f
souhail bouricha <souhailbourigha@gmail.com>
2023-01-21 19:09:02 +0100

    React advanced-hooks

---
+++ 03 React advanced-hooks/03.js
@@ -0,0 +1,43 @@
+// useContext: simple Counter
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+
+// 🐨 create your CountContext here with React.createContext
+const CountContext = React.createContext();
+function CountProvider(){
+  const [count,setCount] = React.useState(0);
+  const value = [count,setCount];
+  return (<CountContext.Provider value={value} ></CountContext.Provider>)
+}
+// 🐨 create a CountProvider component here that does this:
+//   🐨 get the count state and setCount updater with React.useState
+//   🐨 create a `value` array with count and setCount
+//   🐨 return your context provider with the value assigned to that array and forward all the other props
+//   💰 more specifically, we need the children prop forwarded to the context provider
+
+function CountDisplay() {
+  // 🐨 get the count from useContext with the CountContext
+  const [count] = React.useContext(CountContext);
+  return <div>{`The current count is ${count}`}</div>
+}
+
+function Counter() {
+  // 🐨 get the setCount from useContext with the CountContext
+  const [setCount] = React.useContext(CountContext);
+  const increment = () => setCount(c => c + 1)
+  return <button onClick={increment}>Increment count</button>
+}
+
+function App() {
+  return (
+    <div>
+      <CountProvider>
+        <CountDisplay />
+        <Counter />
+      </CountProvider>
+    </div>
+  )
+}
+
+export default App
